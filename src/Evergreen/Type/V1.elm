7e4a49cc98fd140130274384bbbdabeffe64dc4d
Unknown <MartinSStewart@gmail.com>
2020-01-26 16:31:19 +0100

    Add evergreen file

---
+++ src/Evergreen/Type/V1.elm
@@ -0,0 +1,34 @@
+module Evergreen.Type.V1 exposing (..)
+
+import ColorIndex exposing (ColorIndex)
+import Lamdera exposing (ClientId)
+import Set exposing (Set)
+
+
+type alias BackendModel =
+    { clients : Set ClientId
+    , currentColor : ColorIndex
+    }
+
+
+type alias FrontendModel =
+    { currentColor : Maybe ColorIndex
+    }
+
+
+type FrontendMsg
+    = UserPressColor ColorIndex
+    | FNoop
+
+
+type ToBackend
+    = ChooseColor ColorIndex
+    | ClientConnect
+
+
+type BackendMsg
+    = Noop
+
+
+type ToFrontend
+    = UpdateColor ColorIndex
