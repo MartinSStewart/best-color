9d6017af8fa5f89c28a3992998789801e1e10601
Martin Stewart <MartinSStewart@gmail.com>
2021-05-31 21:50:51 +0200

    Add credits for JS code

--- elm-pkg-js/set-favicon.js
+++ elm-pkg-js/set-favicon.js
@@ -1,5 +1,6 @@
 exports.init = async function(app) {
   app.ports.martinsstewart_set_favicon_to_js.subscribe(function(data) {
+    // Original code found here: https://stackoverflow.com/a/260876
     var link = document.querySelector("link[rel~='icon']");
     if (!link) {
         link = document.createElement('link');
