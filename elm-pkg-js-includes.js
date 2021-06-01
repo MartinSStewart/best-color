d371132de1fbe62bfef4714b4a308e3c2adadfc2
Martin Stewart <MartinSStewart@gmail.com>
2021-06-01 09:09:29 +0200

    Fix JS

--- elm-pkg-js-includes.js
+++ elm-pkg-js-includes.js
@@ -8,5 +8,5 @@ exports.init = async function init(app) {
   // @WARNING: this only runs for Lamdera production deploys!
   // This file will not run in Local development, an equivalent to this is
   // automatically generated in Local Development for every file in elm-pkg-js/
-  copyToClipboard.init(app);
+  setFavicon.init(app);
 }
